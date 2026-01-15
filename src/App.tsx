eb6a51ae41b1dd519d678bd52c90e6ba022ae573
Renato <renatofekete.91@gmail.com>
2026-01-15 14:17:29 +0100

    added mock data

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1>Quiz App</h1>
+      <h1 className='text-3xl font-bold underline text-center'>Quiz App</h1>
     </>
   )
 }
---
+++ src/api/data.ts
@@ -0,0 +1,10 @@
+import type { Quiz, Question } from "../types/quiz";
+
+export const MOCK_QUESTIONS: Question[] = [
+  { id: 1, question: "Koji je glavni grad Hrvatske?", answer: "Zagreb" },
+  { id: 2, question: "Tko je napisao 'Povratak Filipa Latinovicza'?", answer: "Miroslav Krleža" }
+];
+
+export const MOCK_QUIZZES: Quiz[] = [
+  { id: 1, title: "Enterwell Opće Znanje", questions: [MOCK_QUESTIONS[0], MOCK_QUESTIONS[1]] }
+];
